ea045640e06c236cc34124c52b59393cd4a35f19
PedroBM <pedrobecmucha12@gmail.com>
2024-11-10 16:20:31 -0500

    a

--- js/index.js
+++ js/index.js
@@ -1,27 +1,24 @@
-// Detectamos cuando el video está a punto de entrar en la vista
 document.addEventListener("DOMContentLoaded", function () {
     const videos = document.querySelectorAll('video[data-src]');
   
     const options = {
-      root: null,  // 'null' significa que usaremos el viewport (ventana del navegador)
+      root: null,  
       rootMargin: '0px',
-      threshold: 0.25  // Cargar cuando el video esté al menos al 25% en la ventana
+      threshold: 0.25  
     };
   
-    // Creamos el observer para detectar la visibilidad del video
+   
     const observer = new IntersectionObserver((entries, observer) => {
       entries.forEach(entry => {
         if (entry.isIntersecting) {
-          // Cuando el video esté en vista, cambiamos el 'data-src' por 'src' y cargamos el video
           const video = entry.target;
           video.src = video.getAttribute('data-src');
-          video.load();  // Esto inicia la carga del video
-          observer.unobserve(video);  // Dejamos de observar este video
+          video.load();  
+          observer.unobserve(video);  
         }
       });
     }, options);
   
-    // Comenzamos a observar todos los videos
     videos.forEach(video => {
       observer.observe(video);
     });
